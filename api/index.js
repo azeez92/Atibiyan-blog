4ff81d2e3cc12226c1d043a7baa79be92e73da2d
azeez92 <azeezsodiq18@gmail.com>
2024-11-24 14:10:16 +0100

    Database setup

--- api/index.js
+++ api/index.js
@@ -1,10 +1,28 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config()
+
+mongoose.connect(
+process.env.MONGO
+
+).then(() => {
+    console.log('Mongodb is connected')
+})
+.catch((err) => {
+    console.log(err)
+})
+
 
 const app = express();
 
 app.listen(3000, () =>{
 
     console.log('Server is running on port 3000')
+}).then
 
 
-})
+// db user: azeezsodiq18
+// db password: UfjjokLZsFa7rq1L
