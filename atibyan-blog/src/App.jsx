2d092d6c24b0cd715d90c293b6f8c3302c94dc13
azeez92 <azeezsodiq18@gmail.com>
2024-11-24 11:24:00 +0100

    nav Bar edit

--- atibyan-blog/src/App.jsx
+++ atibyan-blog/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/projects";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+import Dashboard from "./pages/Dashboard";
+
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+<Route path="/" element={<Home />}/>
+<Route path="/about" element={<About />}/>
+<Route path="/projects" element={<Projects />}/>
+<Route path="/signin" element={<Signin />}/>
+<Route path="/signin" element={<Signup />}/>
+<Route path="/dashboard" element={<Dashboard />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
