62539d17f63ce8db4b66861479df55426d507a9b
azeez92 <azeezsodiq18@gmail.com>
2024-11-24 12:39:21 +0100

    Backend  Setup

--- atibyan-blog/src/App.jsx
+++ atibyan-blog/src/App.jsx
@@ -5,18 +5,21 @@ import Projects from "./pages/projects";
 import Signin from "./pages/signin";
 import Signup from "./pages/signup";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+
 
 
 
 export default function App() {
   return (
     <BrowserRouter>
+   <Header />
     <Routes>
 <Route path="/" element={<Home />}/>
 <Route path="/about" element={<About />}/>
 <Route path="/projects" element={<Projects />}/>
 <Route path="/signin" element={<Signin />}/>
-<Route path="/signin" element={<Signup />}/>
+<Route path="/signup" element={<Signup />}/>
 <Route path="/dashboard" element={<Dashboard />}/>
     </Routes>
     </BrowserRouter>
