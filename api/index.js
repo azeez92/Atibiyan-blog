93571ac2fe710a8d61ea1eeca3dbb8ed99268f20
azeez92 <azeezsodiq18@gmail.com>
2024-11-29 18:16:09 +0100

    create Sign up

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -26,5 +27,5 @@ app.listen(3000, () =>{
 
 
 app.use ('/api/user', userRouter)
-
+app.use ('/api/auth', authRouter)
 
